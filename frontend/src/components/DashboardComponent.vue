801bfe636231b98edd6270051cc82bdcfab5d70f
nakarinh14 <nakarinh14@gmail.com>
2020-07-18 17:35:25 +0700

    Refactor DashboardComponent for reuseable component principle. Add Csmall UI updates

--- frontend/src/components/DashboardComponent.vue
+++ frontend/src/components/DashboardComponent.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         <b>{{this.$cookies.get("firstname")}} {{this.$cookies.get("lastname")}}'s Dashboard</b>
+        <!-- Select term -->
         <h3 v-if="requested_term.semester !== ''">{{requested_term.semester}}, {{requested_term.year}}</h3>
         <b-container fluid>
             <b-row align-h="start">
@@ -8,11 +9,76 @@
                     <b-form-select v-model="selected_term" :options="options" size="sm" class="mt-3"></b-form-select>
                 </b-col>
             </b-row>
-
         </b-container>
 
+        <!-- Table component-->
+        <b-container style="margin-bottom: 20px" fluid>
+            <b-row cols="3">
+                <b-col  class="d-flex justify-content-start">
+                    <div>
+                        <b>Show</b>
+                        <b-form-select
+                                v-model="perPage"
+                                id="perPageSelect"
+                                size="sm"
+                                :options="pageOptions"
+                                style="width: 65px"
+                        ></b-form-select>
+                        <b>entries</b>
 
-        <component :is="component_name" :course="course" :busy="busy" :filter="search_filter"></component>
+                    </div>
+                </b-col>
+                <b-col>
+                    <b-input-group size="sm">
+                        <b-input-group-prepend>
+                                <span class="input-group-text">
+                                    <BIconSearch class="icon-bar"></BIconSearch>
+                                </span>
+                        </b-input-group-prepend>
+                        <b-form-input
+                                v-model="searchFilter"
+                                placeholder="Search Courses"
+                                type="search"
+                                class="search-bar"
+                        >
+                        </b-form-input>
+                    </b-input-group>
+                </b-col>
+            </b-row>
+        </b-container>
+
+        <component
+                :is="component_name"
+                :course="course"
+                :busy="busy"
+                :searchFilter="searchFilter"
+                :currentPage="currentPage"
+                :perPage="perPage"
+                v-on:filterUpdated="updatePagination"
+                v-on:fetchData="fetchData"
+        ></component>
+
+        <b-container style="margin-top: 15px" fluid>
+            <b-row>
+                <b-col></b-col>
+                <b-col>
+                    <span>{{currentPageCount}}</span>
+                </b-col>
+                <b-col>
+                    <b-pagination
+                            v-model="currentPage"
+                            :per-page="perPage"
+                            :total-rows="rows"
+                            aria-controls="my-table"
+                            size="sm"
+                            class="justify-content-end"
+                            prev-text="Prev"
+                            next-text="Next"
+                    ></b-pagination>
+                </b-col>
+            </b-row>
+        </b-container>
+
     </div>
 </template>
 
@@ -43,8 +109,23 @@
                 showed_term: '',
                 options: [],
                 busy: false,
-                search_filter: '',
-                currentPage: 0
+                searchFilter: null,
+                currentPage: 1,
+                perPage: 10,
+                rows: 0,
+                pageOptions: [10, 25, 40, 100]
+            }
+        },
+        computed: {
+            currentPageCount: function(){
+                let minBound = 1 + ((this.currentPage-1)*this.perPage)
+                let maxBound = Math.min(this.currentPage * this.perPage, this.rows);
+                if(this.rows === 0){
+                    minBound = 0;
+                    maxBound = 0;
+                }
+                console.log(minBound)
+                return `Showing ${minBound} to ${maxBound} entries out of ${this.rows}`
             }
         },
         watch:{
@@ -65,34 +146,46 @@
                         }
                     })
                 this.busy = false;
+            },
+            course: function(){
+                this.updatePagination(this.course.length)
+            }
+        },
+        methods:{
+            fetchData: function(){
+                const apiURL = "http://localhost:8081/api/dashboard";
+                this.busy = true;
+                axios.get(apiURL,{
+                    params: {id: this.selected_term},
+                    withCredentials: true
+                })
+                    .then(response => {
+                        if(response.data.role){
+                            this.requested_term = response.data.requestedTerm;
+                            this.course = response.data.courses;
+                            this.options = response.data.termOptions.map(x => ({"value":x.id, "text":x.semester+", "+x.year}));
+                            this.selected_term = response.data.requestedTerm.id;
+                            switch(response.data.role){
+                                case "student":
+                                    this.component_name = "StudentDashboard"
+                                    break;
+                                case "instructor":
+                                    this.component_name = "InstructorDashboard"
+                                    break;
+                            }
+                        }
+                    })
+                    .catch(()=> {
+                        console.log("Dashboard REST call failed.")
+                    })
+            },
+            updatePagination: function(filteredLength){
+                this.rows = filteredLength
+                this.currentPage = 1
             }
         },
         created() {
-            const apiURL = "http://localhost:8081/api/dashboard";
-            this.busy = true;
-            axios.get(apiURL,{
-                params: {id: this.selected_term},
-                withCredentials: true
-            })
-                .then(response => {
-                    if(response.data.role){
-                        this.requested_term = response.data.requestedTerm;
-                        this.course = response.data.courses;
-                        this.options = response.data.termOptions.map(x => ({"value":x.id, "text":x.semester+", "+x.year}));
-                        this.selected_term = response.data.requestedTerm.id;
-                        switch(response.data.role){
-                            case "student":
-                                this.component_name = "StudentDashboard"
-                                break;
-                            case "instructor":
-                                this.component_name = "InstructorDashboard"
-                                break;
-                        }
-                    }
-                })
-                .catch(()=> {
-                    console.log("Dashboard REST call failed.")
-                })
+            this.fetchData();
         }
     }
 </script>
--- frontend/src/components/StudentDashboard.vue
+++ frontend/src/components/StudentDashboard.vue
@@ -1,46 +1,12 @@
 <template>
     <div>
-        <b-container style="margin-bottom: 20px" fluid>
-            <b-row cols="3">
-                <b-col  class="d-flex justify-content-start">
-                    <div>
-                        <b>Show</b>
-                            <b-form-select
-                                    v-model="perPage"
-                                    id="perPageSelect"
-                                    size="sm"
-                                    :options="pageOptions"
-                                    style="width: 65px"
-                            ></b-form-select>
-                        <b>entries</b>
-
-                    </div>
-                </b-col>
-                <b-col>
-                    <b-input-group size="sm">
-                        <b-input-group-prepend>
-                                <span class="input-group-text">
-                                    <BIconSearch class="icon-bar"></BIconSearch>
-                                </span>
-                        </b-input-group-prepend>
-                        <b-form-input
-                                v-model="search_filter"
-                                placeholder="Search Courses"
-                                type="search"
-                                class="search-bar"
-                        >
-                        </b-form-input>
-                    </b-input-group>
-                </b-col>
-            </b-row>
-        </b-container>
         <b-container fluid>
             <b-table striped bordered hover
                      :busy="busy"
                      :head-variant="'dark'"
                      :items="course"
                      :fields="fields"
-                     :filter="search_filter"
+                     :filter="searchFilter"
                      :current-page="currentPage"
                      :per-page="perPage"
                      @filtered="onFiltered"
@@ -61,7 +27,11 @@
                         </template>
                         {{row.item.info}}
                     </b-modal>
-                    <b-button v-b-modal="row.item.id+'-modal'" size="sm" class="mr-2">
+                    <b-button v-b-modal="row.item.id+'-modal'"
+                              size="sm"
+                              class="mr-2"
+                              variant="primary"
+                    >
                         Show Info
                     </b-button>
                 </template>
@@ -71,26 +41,6 @@
                 </template>
             </b-table>
         </b-container>
-        <b-container style="margin-top: 15px" fluid>
-            <b-row>
-                <b-col></b-col>
-                <b-col>
-                    <span>{{currentPageCount}}</span>
-                </b-col>
-                <b-col>
-                    <b-pagination
-                            v-model="currentPage"
-                            :per-page="perPage"
-                            :total-rows="rows"
-                            aria-controls="my-table"
-                            size="sm"
-                            class="justify-content-end"
-                            prev-text="Prev"
-                            next-text="Next"
-                    ></b-pagination>
-                </b-col>
-            </b-row>
-        </b-container>
     </div>
 </template>
 
@@ -98,25 +48,7 @@
 <script>
     export default {
         name: 'StudentDashboard',
-        props: ['course', 'busy'],
-        computed: {
-          currentPageCount: function(){
-              let minBound = 1 + ((this.currentPage-1)*this.perPage)
-              let maxBound = Math.min(this.currentPage * this.perPage, this.rows);
-              if(this.rows === 0){
-                  minBound = 0;
-                  maxBound = 0;
-              }
-              console.log(minBound)
-              return `Showing ${minBound} to ${maxBound} entries out of ${this.rows}`
-          }
-        },
-        watch:{
-            course: function(){
-                this.rows = this.course.length
-                this.currentPage = 1
-            }
-        },
+        props: ['course', 'busy', 'searchFilter', 'currentPage', 'perPage'],
         data() {
             return {
                 fields: [
@@ -132,23 +64,17 @@
                     {key:"infos", label:"Info"}
                 ],
                 info: "",
-                search_filter: null,
-                currentPage: 1,
-                perPage: 10,
-                rows: 0,
-                pageOptions: [10, 25, 40, 100]
-
             }
         },
         methods:{
-            onFiltered: function(filteredItems) {
-                // Trigger pagination to update the number of buttons/pages due to filtering
-                this.rows = filteredItems.length
-                this.currentPage = 1
+            onFiltered: function(filteredItems) { // Trigger pagination to update the number of buttons/pages due to filtering
+                // Emit event back to DashboardComponent to update pagination with filtered length
+                this.$emit('filterUpdated', filteredItems.length)
             }
         }
     };
 </script>
+
 
 
 
