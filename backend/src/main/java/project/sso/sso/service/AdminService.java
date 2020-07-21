ad03290f173c6752af3e47c2cea551186fd8bfe7
williampariwat <william.pariwat@gmail.com>
2020-07-21 14:33:33 +0700

    Adding new user

--- backend/src/main/java/project/sso/sso/service/AdminService.java
+++ backend/src/main/java/project/sso/sso/service/AdminService.java
@@ -2,14 +2,13 @@ package project.sso.sso.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import project.sso.sso.entity.Course;
 import project.sso.sso.entity.Profile;
 import project.sso.sso.entity.Role;
 import project.sso.sso.entity.User;
 import project.sso.sso.model.AddUserRequest;
 import project.sso.sso.model.ValidateResponse;
-import project.sso.sso.repository.ProfileRepository;
-import project.sso.sso.repository.RoleRepository;
-import project.sso.sso.repository.UserRepository;
+import project.sso.sso.repository.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,8 +25,24 @@ public class AdminService {
     @Autowired
     RoleRepository roleRepository;
 
-    public List<User> getAllUsers() {
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    TermRepository termRepository;
+
+    public List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    public List<Course> getAllCourses(Long id){
+        String role = userRepository.getRoleOfId(id).toLowerCase();
+        if(role.equals("student")){
+            return courseRepository.findCourseByStudentId(id);
+        } else if (role.equals("instructor")){
+            return courseRepository.findAllByInstructorId(id);
+        }
+        return null;
     }
 
     public ValidateResponse addUser(AddUserRequest addUserRequest) {
@@ -56,6 +71,5 @@ public class AdminService {
             return new ValidateResponse("Fail");
         }
     }
+
 }
-
-
